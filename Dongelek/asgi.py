1c15efce3090eeb320e0d3aa7e1dc94d7e299d2c
BaUka0 <bauirzhan.bisanov@gmail.com>
2025-05-22 09:21:23 +0500

    Merge remote-tracking branch 'origin/master'

--- Dongelek/asgi.py
+++ Dongelek/asgi.py
@@ -1,11 +1,15 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dongelek.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dongelek.settings')
+from Dongelek.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- Dongelek/routing.py
+++ Dongelek/routing.py
@@ -2,11 +2,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
 import chat.consumers
-import notifications.consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<conversation_id>\w+)/$', chat.consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/notifications/$', notifications.consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/notifications/$', chat.consumers.NotificationConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
